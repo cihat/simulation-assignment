7087922a77263aaeb7db6d8bd77759eae7a3cc59
cihat <cihat02020202@gmail.com>
2022-05-24 20:24:28 +0300

    Update TIME constant

--- src/constans.ts
+++ src/constans.ts
@@ -1,4 +1,4 @@
-const POISSION_RATE = 12 * 60 * 1000; // 12 minutes
+const POISSION_RATE = 3 * 60 * 1000; // 12 minutes
 const POISON_DURATION = 10 * 60 * 1000; // 10 minutes
 const TIME = 3 * 60 * 60 * 1000; // 3 hours
 const computerCount = TIME / POISON_DURATION;
